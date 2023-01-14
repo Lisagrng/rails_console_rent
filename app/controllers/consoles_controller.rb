9e2dbd58b79742ce6b2a17e0203a2803b4e048c9
Lisa Grange <lisa.grange42@gmail.com>
2023-01-14 16:38:49 +0100

    Controller console done and show, index views update

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,6 +1,11 @@
 class ConsolesController < ApplicationController
+  before_action :console_id, only: %i[show edit update destroy]
+
   def index
     @consoles = Console.all
+  end
+
+  def show
   end
 
   def new
@@ -8,17 +13,32 @@ class ConsolesController < ApplicationController
   end
 
   def create
-  end
-
-  def show
+    @console = Console.new(console_params)
+    @console.user = current_user
+    @console.save
+    redirect_to console_path(@console)
   end
 
   def edit
   end
 
   def update
+    @console.update(console_params)
+    redirect_to console_path(@console)
   end
 
   def destroy
+    @console.destroy
+    redirect_to consoles_path, status: :see_other
+  end
+
+  private
+
+  def console_id
+    @console = Console.find(params[:id])
+  end
+
+  def console_params
+    params.require(:console).permit(:name, :year, :description)
   end
 end
