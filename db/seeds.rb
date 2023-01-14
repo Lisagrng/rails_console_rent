06ac4786f5181f62bfe78d8695447dedce6f117c
Lisa Grange <lisa.grange42@gmail.com>
2023-01-14 14:34:16 +0100

    Avancement seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create
-Console.create
-Booking.create
+Booking.destroy_all
+Console.destroy_all
+User.destroy_all
+
+users = []
+10.times do |i|
+  user = User.create(email: "lisa_#{i}@lisa.com", password: '123456')
+  users << user
+end
+
+users.each do |user|
+  Console.create(name: 'PS1', year: 2005, description: 'old console', user_id: user.id)
+end
+
+Booking.create!(console_id: Console.last.id, user_id: User.last.id)
